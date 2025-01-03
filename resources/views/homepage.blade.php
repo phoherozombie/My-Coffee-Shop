77d0710f43eefe895185297e7c968c6941537e91
phoherozombie <phucdo3687076@gmail.com>
2025-01-03 11:48:46 +0700

    update UI and controller

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,4 +8,14 @@
     @foreach ($categories as $category)
         <x-product-grids :category="$category" :products="$category['products']"/>
     @endforeach
+        {{-- add more space for avoid cart bar override content --}}
+        <div class="w-full min-h-[150px]"></div>
+    
+    <x-slot name="components">
+         <livewire:checkout.cart-bar />
+ 
+         <livewire:checkout.add-cart-item-popup />
+     </x-slot>
+ 
+ 
 </x-user-layout>
