5a0e576dfc9deeedccb2c4fb9709a2823c4ea2ec
phoherozombie <phucdo3687076@gmail.com>
2025-01-03 12:02:09 +0700

    update lang

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,19 @@
+use App\Events\OrderCreated;
+   use App\Listeners\SendOrderCreatedNotification;
+  
+   …
+
+   /**
+     * The event to listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
+    protected $listen = [
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
+        OrderCreated::class => [
+            SendOrderCreatedNotification::class,
+        ],
+    ];
+
